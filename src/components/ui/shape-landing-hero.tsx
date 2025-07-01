dc00d5e8cf98c95ef3600f88bae814229891756b
Inzamam Shaikh <92295764+456535688765@users.noreply.github.com>
2025-07-01 19:04:35 +0530

    first commit & Background

---
+++ src/components/ui/shape-landing-hero.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { motion, useMotionValue, useTransform, animate } from "framer-motion";
+import { Circle } from "lucide-react";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+import ElegantShape from "./ElegantShape";
+import { elegantShapes } from "@/config/ElegantShapeConfig";
+
+function HeroGeometric() {
+  const fadeUpVariants = {
+    hidden: { opacity: 0, y: 30 },
+    visible: (i: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 1,
+        delay: 0.5 + i * 0.2,
+        ease: [0.25, 0.4, 0.25, 1],
+      },
+    }),
+  };
+
+  return (
+    <div className="relative min-h-screen w-full flex items-center justify-center overflow-hidden bg-[#030303]">
+      <div className="absolute inset-0 bg-gradient-to-br from-indigo-500/[0.05] via-transparent to-rose-500/[0.05] blur-3xl" />
+
+      <div className="absolute inset-0 overflow-hidden">
+        {elegantShapes.map((shape, index) => (
+          <ElegantShape
+            key={index}
+            delay={shape.delay}
+            width={shape.width}
+            height={shape.height}
+            rotate={shape.rotate}
+            gradient={shape.gradient}
+            className={shape.className}
+          />
+        ))}
+      </div>
+
+      <div className="relative z-10 container mx-20 px-4 md:px-6 bg-amber-300">
+        <div className="max-w-3xl mx-auto text-center bg-red-400">
+          <motion.div custom={1} initial="hidden" animate="visible">
+            <h1 className="text-4xl sm:text-6xl md:text-8xl font-bold mb-6 md:mb-8 tracking-tight">
+              <span className="bg-clip-text text-transparent bg-gradient-to-b from-white to-white/80">
+                Hello
+              </span>
+              <br />
+              <span
+                className={cn(
+                  "bg-clip-text text-transparent bg-gradient-to-r from-indigo-300 via-white/90 to-rose-300 "
+                )}
+              >
+                hello
+              </span>
+            </h1>
+          </motion.div>
+
+          <motion.div custom={2} initial="hidden" animate="visible">
+            <p className="text-base sm:text-lg md:text-xl text-white/40 mb-8 leading-relaxed font-light tracking-wide max-w-xl mx-auto px-4"></p>
+          </motion.div>
+        </div>
+      </div>
+
+      <div className="absolute inset-0 bg-gradient-to-t from-[#030303] via-transparent to-[#030303]/80 pointer-events-none" />
+    </div>
+  );
+}
+
+export { HeroGeometric };
