5d927488d8780a7d3e2bae3087fb544d69e33da2
Inzamam Shaikh <92295764+456535688765@users.noreply.github.com>
2025-07-15 20:24:05 +0530

    small content fix

--- src/config/OurWorkSection.ts
+++ src/config/OurWorkSection.ts
@@ -1,47 +1,35 @@
 export const items = [
   {
     id: "item-1",
-    title: "Build Modern UIs",
-    summary:
-      "Create stunning user interfaces with our comprehensive design system.",
+    title: "Just Chicken",
+    summary: "A restaurant website for crispy chicken",
     url: "#",
     videoURL:
       "https://res.cloudinary.com/dxnnjfduu/video/upload/v1752509020/Untitled_design_23_hwd6js.mp4",
   },
   {
     id: "item-2",
-    title: "Design System Components",
-    summary:
-      "Explore our library of customizable components built with shadcn/ui and Tailwind CSS.",
+    title: "Foody",
+    summary: "A restaurant website for all kind of Italian cuisine",
     url: "#",
     videoURL:
       "https://res.cloudinary.com/dxnnjfduu/video/upload/v1752510055/Untitled_design_21_ncwwos.mp4",
   },
   {
     id: "item-3",
-    title: "Responsive Layouts",
-    summary:
-      "Build websites that look great on any device with our responsive design patterns.",
+    title: "Make Home",
+    summary: "A real-estate website for searching homes and properties",
     url: "#",
     videoURL:
       "https://res.cloudinary.com/dxnnjfduu/video/upload/v1752510501/Untitled_design_22_jtg0er.mp4",
   },
   {
     id: "item-4",
-    title: "Developer Experience",
+    title: "Softsell",
     summary:
-      "Streamline your workflow with our developer-friendly tools and documentation.",
+      "A website made for business which monetizes unused software licenses",
     url: "#",
     videoURL:
       "https://res.cloudinary.com/dxnnjfduu/video/upload/v1752510515/Untitled_design_24_sufte9.mp4",
-  },
-  {
-    id: "item-5",
-    title: "Performance First",
-    summary:
-      "Create fast, optimized websites using our performance-focused components.",
-    url: "#",
-    videoURL:
-      "https://www.shadcnblocks.com/images/block/placeholder-dark-1.svg",
   },
 ];
